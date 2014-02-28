ff2221fb8e8ec351fda6948f5f7f9a117c4f661f
Victor Ilisei <vilisei@Victors-MacBook-Pro.local>
2014-02-27 23:00:50 -0700

    Created PSBN Radio Controllers

---
+++ PSBN/PSBN/PSBNRadio.h
@@ -0,0 +1,13 @@
+//
+//  PSBNRadio.h
+//  PSBN
+//
+//  Created by Victor Ilisei on 2/27/14.
+//  Copyright (c) 2014 Tech Genius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSBNRadio : UICollectionViewController
+
+@end
---
+++ PSBN/PSBN/PSBNRadio.m
@@ -0,0 +1,30 @@
+//
+//  PSBNRadio.m
+//  PSBN
+//
+//  Created by Victor Ilisei on 2/27/14.
+//  Copyright (c) 2014 Tech Genius. All rights reserved.
+//
+
+#import "PSBNRadio.h"
+
+@interface PSBNRadio ()
+
+@end
+
+@implementation PSBNRadio
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = @"Radio";
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+@end

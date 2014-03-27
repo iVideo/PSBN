d82d624bd62f23d0b0dfb67a18541b6ec1c92b9e
Victor Ilisei <vilisei@Victors-MacBook-Pro.local>
2014-03-27 13:18:41 -0700

    Created subclass for future scoreboard cells

---
+++ PSBN/PSBN/PSBNScoreboard.h
@@ -0,0 +1,11 @@
+//
+//  PSBNScoreboard.h
+//  PSBN
+//
+//  Created by Victor Ilisei on 3/27/14.
+//  Copyright (c) 2014 Tech Genius. All rights reserved.
+//
+
+@interface PSBNScoreboard : UICollectionViewCell
+
+@end
---
+++ PSBN/PSBN/PSBNScoreboard.m
@@ -0,0 +1,13 @@
+//
+//  PSBNScoreboard.m
+//  PSBN
+//
+//  Created by Victor Ilisei on 3/27/14.
+//  Copyright (c) 2014 Tech Genius. All rights reserved.
+//
+
+#import "PSBNScoreboard.h"
+
+@implementation PSBNScoreboard
+
+@end

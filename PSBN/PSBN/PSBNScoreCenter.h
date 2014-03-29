c04156c0b75e8c2e11b96bb3a45f046d1721fee3
Victor Ilisei <vilisei@Victors-MacBook-Pro.local>
2014-03-29 13:28:32 -0700

    Correct import and declarations

--- PSBN/PSBN/PSBNScoreCenter.h
+++ PSBN/PSBN/PSBNScoreCenter.h
@@ -6,8 +6,22 @@
 //  Copyright (c) 2014 Tech Genius. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <MessageUI/MessageUI.h>
+#import "PSBNRadio.h"
 
-@interface PSBNScoreCenter : UICollectionViewController
+@interface PSBNScoreCenter : UICollectionViewController <UINavigationControllerDelegate, MFMailComposeViewControllerDelegate, UIPopoverControllerDelegate> {
+    UIActionSheet *composeActionSheet;
+    
+    UIPopoverController *showRadioFrame;
+    
+    NSMutableArray *games;
+    
+    NSTimer *refreshTimer;
+}
+
+- (void)refresh;
+- (IBAction)resetTimer:(id)sender;
+- (IBAction)chooseSubmit:(id)sender;
+- (IBAction)showRadio:(id)sender;
 
 @end
